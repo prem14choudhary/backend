fd6e2f0e4f5e96dd823edb4e306cf04a150953fe
Vineet Pruthi <vineetpruthi@Vineets-MacBook-Air.local>
2025-07-09 14:38:54 +0530

    hsa

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 1700;
+
+app.use(cors());
+
+app.get('/api/ping', (req, res) => {
+  res.json({ message: 'Pong from backend!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`);
+});
